19f134279b085116259951aa04165b93465587d8
Ivan Kirov <kirov.i.ivan@gmail.com>
2026-05-23 11:12:17 +0300

    nbtg

--- guess_a_number.py
+++ guess_a_number.py
@@ -2,6 +2,24 @@
 
 import random
 
-for i in range(100):
-    x=random.randint(1,100)
-    print(x,end=', ')
+x=random.randint(1,100)
+
+print("I will choose a random number. You place your guesses. I will guide you. Good лук!")
+
+while 1:
+    a=input('Guess the number [1,100]: ')
+
+    if not a.isdigit():
+        print('A number [1,100]!!!!')
+        continue
+    else:
+        a=int(a)
+
+    if a==x:
+        print('You guessed it!')
+        break
+    elif a<x:
+        print('Too low!')
+    else:
+        print('Too high!')
+
